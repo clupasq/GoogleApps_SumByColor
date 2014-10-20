b381c7b24880f8dcb4c482ef7f785e4ef7d45586
Juan Hoyos <juan.hoyosr@gmail.com>
2014-10-19 21:05:49 -0500

    Added remote spreadsheets support

--- source.js
+++ source.js
@@ -1,39 +1,66 @@
-function getBackgroundColor(rangeSpecification) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet();
+function _getSpreadsheet(key) {
+  return key ? SpreadsheetApp.openById(key) : SpreadsheetApp.getActiveSpreadsheet();
+}
+
+function getBackgroundColor(rangeSpecification, key) {
+  var sheet = _getSpreadsheet(key);
   return sheet.getRange(rangeSpecification).getBackground();
 }
 
-function getForegroundColor(rangeSpecification) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet();
+function getForegroundColor(rangeSpecification, key) {
+  var sheet = _getSpreadsheet(key);
   return sheet.getRange(rangeSpecification).getFontColor();
 }
 
 
-function sumWhereBackgroundColorIs(color, rangeSpecification) {
-  var condition = function (cell) { return cell.getBackground() == color; };
-  return sumByCondition(color, rangeSpecification, condition);
+function sumWhereBackgroundColorIs(color, rangeSpecification, key) {
+  var condition = function (cell) { return cell.getBackground().toLowerCase() == color.toLowerCase(); };
+  return sumByCondition(rangeSpecification, condition, key);
 }
 
-function sumWhereBackgroundColorIsNot(color, rangeSpecification) {
-  var condition = function (cell) { return cell.getBackground() != color; };
-  return sumByCondition(color, rangeSpecification, condition);
+/**
+ * Sums all the values of cells with a given background color.
+ *
+ * @param {color}
+ * @param {rangeSpecification} rangeSpecification - the range to search against.
+ * @param {key} key (optional) - the key of a remote spreadsheet. If provided the
+ *        range lookup will be done remotely.
+ * @return A sum of the cell values.
+ * @customfunction
+ */
+function sumWhereBackgroundColorIsNot(color, rangeSpecification, key) {
+  var condition = function (cell) {
+    return cell.getBackground().toLowerCase() != color.toLowerCase();
+  };
+  return sumByCondition(rangeSpecification, condition, key);
 }
 
-function sumWhereForegroundColorIs(color, rangeSpecification) {
-  var condition = function (cell) { return cell.getFontColor() == color; };
-  return sumByCondition(color, rangeSpecification, condition);
+function sumWhereForegroundColorIs(color, rangeSpecification, key) {
+  var condition = function (cell) { return cell.getFontColor().toLowerCase() == color.toLowerCase(); };
+  return sumByCondition(rangeSpecification, condition, key);
 }
 
-function sumWhereForegroundColorIsNot(color, rangeSpecification) {
-  var condition = function (cell) { return cell.getFontColor() != color; };
-  return sumByCondition(color, rangeSpecification, condition);
+function sumWhereForegroundColorIsNot(color, rangeSpecification, key) {
+  var condition = function (cell) { return cell.getFontColor().toLowerCase() != color.toLowerCase(); };
+  return sumByCondition(rangeSpecification, condition, key);
 }
 
 
 
-
-function sumByCondition(color, rangeSpecification, condition) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet();
+/**
+ * Sums all the values of cells in the given range that has a
+ * specific condition.
+ *
+ * @param {rangeSpecification} rangeSpecification - the range to search against.
+ * @param {condition} condition - a function that determines if the cell should be
+ *        summed or not.
+ * @param {key} key (optional) - the key of a remote spreadsheet. If provided the
+ *        range lookup will be done remotely.
+ * @return A sum of the cell values.
+ * @customfunction
+ */
+function sumByCondition(rangeSpecification, condition, key) {
+  var sheet = _getSpreadsheet(key);
   var range = sheet.getRange(rangeSpecification);
   
   var sum = 0;
@@ -44,7 +71,7 @@ function sumByCondition(color, rangeSpecification, condition) {
       var cell = range.getCell(i, j);
       
       if(condition(cell))
-        sum += parseFloat(cell.getValue());
+        sum += parseFloat(cell.getValue() || 0);
     }
   }
   
